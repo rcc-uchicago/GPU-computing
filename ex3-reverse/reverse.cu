c947bebc9a4d18eaf79eeabf7e1142250ae00b34
Trung Nguyen <ndactrung@gmail.com>
2024-03-25 10:08:20 -0500

    Added examples for reverse (shared mem) and reduction

--- ex3-reverse/reverse.cu
+++ ex3-reverse/reverse.cu
@@ -3,18 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-__global__ void staticReverse(int *d, int n)
-{
-  __shared__ int s[64];
-
-  int t = threadIdx.x;
-  int tr = n-t-1;
-  s[t] = d[t];
-  __syncthreads();
-  d[t] = s[tr];
-}
-
-__global__ void dynamicReverse(int *d, int n)
+__global__ void reverse_kernel(int *d, int n)
 {
   extern __shared__ int s[];
 
@@ -25,52 +14,53 @@ __global__ void dynamicReverse(int *d, int n)
   d[t] = s[tr];
 }
 
-int main(void)
+int main(int argc, char** argv)
 {
-  const int n = 64;
+  // parse command line arguments
+  if (argc != 2) {
+    printf("Usage: ./a.out [Number of elements] \n");
+    return 0;
+  }
+
+  const int n = atoi(argv[1]);
   int a[n], r[n], d[n];
 
   for (int i = 0; i < n; i++) {
     a[i] = i;
-    r[i] = n-i-1;
+    r[i] = n-i-1; // correct values for testing
     d[i] = 0;
   }
 
+  printf("In-place reverse an array with %d elements..\n",  n);
+
   int *d_d;
-  cudaMalloc(&d_d, n * sizeof(int)); 
+  cudaMalloc(&d_d, n * sizeof(int));
 
-  // run version with static shared memory
+  int passed = 1;
+
+  // copy from a from HOST to DEVICE
   cudaMemcpy(d_d, a, n*sizeof(int), cudaMemcpyHostToDevice);
 
-  staticReverse<<<1, n>>>(d_d, n);
+  // launch the kernel
+  reverse_kernel<<<1, n, n*sizeof(int)>>>(d_d, n);
 
-  cudaMemcpy(d, d_d, n*sizeof(int), cudaMemcpyDeviceToHost);
-
-  // check the result
-  for (int i = 0; i < n; i++) {
-    if (d[i] != r[i]) {
-      printf("Error: d[%d]!=r[%d] (%d, %d)n", i, i, d[i], r[i]);
-      exit(EXIT_FAILURE);
-    }
-  }
-
-  // run dynamic shared memory version
-  cudaMemcpy(d_d, a, n*sizeof(int), cudaMemcpyHostToDevice);
-
-  dynamicReverse<<<1, n, n*sizeof(int)>>>(d_d, n);
-
+  // copy from d from DEVICE to HOST
   cudaMemcpy(d, d_d, n * sizeof(int), cudaMemcpyDeviceToHost);
 
-  // check the result  
+  // check the result
+  passed = 1;
   for (int i = 0; i < n; i++) {
     if (d[i] != r[i]) {
-       printf("Error: d[%d]!=r[%d] (%d, %d)n", i, i, d[i], r[i]);
-       exit(EXIT_FAILURE);
+       printf("Error: d[%d] != r[%d] (%d, %d)\n", i, i, d[i], r[i]);
+       passed = 0;
+       break;
     }
   }
 
   cudaFree(d_d);
 
-  printf("Test PASSED\n");
+  if (passed == 1) printf("Test PASSED\n");
+  else printf("Test FAILED\n");
+
   return 0;
 }
